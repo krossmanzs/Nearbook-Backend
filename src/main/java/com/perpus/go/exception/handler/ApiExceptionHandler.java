8555755fca212d701749783e40530d21b38aa667
Krossmanzs <radi.adi78@gmail.com>
2022-10-19 17:14:18 +0700

    Disable Exception Handler extending from ResponseEntityExceptionHandler

--- src/main/java/com/perpus/go/exception/handler/ApiExceptionHandler.java
+++ src/main/java/com/perpus/go/exception/handler/ApiExceptionHandler.java
@@ -1,31 +1,46 @@
 package com.perpus.go.exception.handler;
 
 import com.perpus.go.exception.BadRequestException;
+import com.perpus.go.exception.NotFoundException;
 import com.perpus.go.exception.error.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApiExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
     @ExceptionHandler(value = {BadRequestException.class})
     public ResponseEntity<Object> handleBadRequestException(
             BadRequestException e,
             WebRequest request
     ) {
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
-        ErrorDetails errorDetails = new ErrorDetails(
-                badRequest,
+        ErrorDetails errorDetails = generateErrorDetails(HttpStatus.BAD_REQUEST, e, request);
+        return new ResponseEntity<>(errorDetails, errorDetails.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(
+            BadRequestException e,
+            WebRequest request
+    ) {
+        ErrorDetails errorDetails = generateErrorDetails(HttpStatus.NOT_FOUND,e, request);
+        return new ResponseEntity<>(errorDetails, errorDetails.getHttpStatus());
+    }
+
+    private ErrorDetails generateErrorDetails(
+            HttpStatus status,
+            Throwable throwable,
+            WebRequest request
+    ) {
+        return new ErrorDetails(
+                status,
                 ZonedDateTime.now(),
-                e.getMessage(),
+                throwable.getMessage(),
                 request.getDescription(false)
         );
-
-        return new ResponseEntity<>(errorDetails, badRequest);
     }
 }
